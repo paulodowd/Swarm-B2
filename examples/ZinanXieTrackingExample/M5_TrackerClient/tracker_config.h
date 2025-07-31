90520fb139f8e79a4aa9e19b7f3f20bde4981cfd
paulodowd <paul.j.odowd@gmail.com>
2025-07-31 10:51:10 +0100

    update

---
+++ examples/ZinanXieTrackingExample/M5_TrackerClient/tracker_config.h
@@ -0,0 +1,17 @@
+// Connection information for the tracking
+// system.
+const char * server_ip = "192.168.1.200";
+const uint16_t server_port = 5000;
+
+
+// The tcp/ip packet for the tracking
+// system.
+#pragma pack(push,1)
+typedef struct tracker_packet {
+  int16_t marker_id; // 2 byte
+  float x;          // 4 bytes
+  float y;          // 4 bytes
+  float theta;      // 4 bytes
+  int16_t valid;     // 2 byte
+} tracker_packet_t; // = 14 bytes
+#pragma pack(pop)
